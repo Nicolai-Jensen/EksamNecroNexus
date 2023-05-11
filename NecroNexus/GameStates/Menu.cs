a224615dc32d68dd7b49094fd3fd9aaef35331c8
Nicolai-Jensen <nicolaijensen11@gmail.com>
2023-05-11 11:03:35 +0200

    First push
    Fixed Folder referencing and added States functions

--- NecroNexus/GameStates/Menu.cs
+++ NecroNexus/GameStates/Menu.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NecroNexus.GameStates
+namespace NecroNexus
 {
+
+    //--------------------------Nicolai Jensen----------------------------//
+
     class Menu : State
     {
+        //A Texture variable for our background
+        private Texture2D[] backgroundsprite;
+
+        //2 variables to control key presses
+        private KeyboardState currentKey;
+        private KeyboardState previousKey;
+
+        /// <summary>
+        /// The States Constructor which applies the picture that is shown
+        /// </summary>
+        public Menu()
+        {
+            
+        }
+
+        public override void Initialize()
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+
+        }
+
+        /// <summary>
+        /// A way to ensure single key presses when taking action
+        /// </summary>
+        public override void Update()
+        {
+            previousKey = currentKey;
+            currentKey = Keyboard.GetState();
+        }
+
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(SpriteSortMode.FrontToBack);
+
+
+            spriteBatch.End();
+        }
     }
 }
--- NecroNexus/GameWorld.cs
+++ NecroNexus/GameWorld.cs
@@ -9,24 +9,74 @@ namespace NecroNexus
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //State variables to reference and swap states
+        private State currentState;
+        private State nextState;
+        private State nextState2;
+        private bool gameStarted = false;
+
+        private Menu menu;
+        private StartScreen startScreen;
+        private LevelOne levelOne;
+
+        //A Vector2 to save our screen size on
+        private static Vector2 screenSize;
+
+        //A property variable for DeltaTime
+        public static float DeltaTime { get; private set; }
+
+        //Properties
+        public static Vector2 ScreenSize
+        {
+            get
+            {
+                return screenSize;
+            }
+        }
+
+        public bool GameStarted
+        {
+            get { return gameStarted; }
+            set { gameStarted = value; }
+        }
+
+        //A property used to get access to what the currentState is in other classes
+        public State CurrentState
+        {
+            get
+            {
+                return currentState;
+            }
+        }
+
         public GameWorld()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            //Sets the games ScreenSize and applies it to our variable
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+
+            screenSize = new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
         }
+
+
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            menu = new Menu();
+            startScreen = new StartScreen();
+            levelOne = new LevelOne();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            ChangeState(menu);
             // TODO: use this.Content to load your game content here
         }
 
@@ -35,7 +85,34 @@ namespace NecroNexus
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Applies a simulation of time to DeltaTime
+            DeltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //An if statement that changesstates if the ChangeState method has been used
+            if (nextState != null)
+            {
+                //changes state into the next one
+                currentState = nextState;
+
+                //Calls initialize and loadContent in the new state
+                currentState.Initialize();
+                currentState.LoadContent();
+                //sets nextState to null
+                nextState = null;
+            }
+
+            if (nextState2 != null)
+            {
+                //changes state into the next one
+                currentState = nextState2;
+
+                //sets nextState to null
+                nextState2 = null;
+            }
+
+
+            //calls the currentstates update
+            currentState.Update();
 
             base.Update(gameTime);
         }
@@ -47,6 +124,21 @@ namespace NecroNexus
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// A Method for Changing which state you are in
+        /// </summary>
+        /// <param name="state">New State</param>
+        public void ChangeState(State state)
+        {
+            //Sets NextState to a State
+            nextState = state;
+        }
+
+        public void ChangeState2(State state)
+        {
+            nextState2 = state;
         }
     }
 }
