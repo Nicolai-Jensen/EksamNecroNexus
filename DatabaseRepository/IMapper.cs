e88d0286e3d74d515decc592c191a3054a4710c3
BjarkeAR <91613897+BjarkeAR@users.noreply.github.com>
2023-05-15 13:17:34 +0200

    Stuff updated
    yo

--- DatabaseRepository/IMapper.cs
+++ DatabaseRepository/IMapper.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using DbDomain;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DatabaseRepository
 {
-    internal interface IMapper
+    public interface IMapper
     {
+        List<User> MapUserFromReader(SQLiteDataReader reader);
+
+        List<Tower> MapTowerFromReader(SQLiteDataReader reader);
+
+        List<TowerSave> MapTowerSaveFromReader(SQLiteDataReader reader);
+
+        List<Level> MapLevelFromReader(SQLiteDataReader reader);
+
     }
 }
