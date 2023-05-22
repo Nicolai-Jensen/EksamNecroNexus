44398cad1ce656026fb5ba23fa0aef75b8b5dd2b
BjarkeAR <91613897+BjarkeAR@users.noreply.github.com>
2023-05-22 10:27:20 +0200

    yo
    yo

---
+++ NecroNexus/Graph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecroNexus
+{
+    internal class Graph<T>
+    {
+        /// <summary>
+        /// NODE LIST
+        /// The list containing our nodes, built into a property.
+        /// Every class can now access the list, but only Graph can manipulate, as the setter is private.
+        /// </summary>
+
+        public List<Node<T>> NodesList { get; private set; } = new List<Node<T>>();
+
+    }
+}
---
+++ NecroNexus/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecroNexus
+{
+    internal class Node<T>
+    {
+
+        public T Data { get; private set; } 
+
+        // EDGE LIST
+        public List<Edge<T>> EdgesList { get; private set; } = new List<Edge<T>>();
+
+        //Constructor 
+        public Node(T data) 
+
+
+    }
+}
