64835d32f80a50998c2529b105e1ddff9d12dcc3
Kasp040g <kasperlaks@gmail.com>
2023-05-24 12:33:56 +0200

    iojygu

--- NecroNexus/ComponentPattern/Summon.cs
+++ NecroNexus/ComponentPattern/Summon.cs
@@ -15,6 +15,7 @@ namespace NecroNexus
         public float AttackRangeRadius { get; set; }
 
         public List<GameObject> enemiesInRange = new List<GameObject>();
+        public Enemy CurrentTarget { get; set; }
 
 
 
@@ -56,42 +57,45 @@ namespace NecroNexus
 
         public virtual void Attack(Enemy enemy)
         {
-
+            if(CurrentTarget != null)
+            {
+                //attack
+            }
         }
 
         /// <summary>
         /// Compares how far each of the enemies inside the range is along the track.the enemy furthest in the track, becomes the current target.
         /// </summary>
-        private Enemy FindFurthestEnemyInRange()
-        {
-            //puts all enemies in range of the tower, in a list
-            List<Enemy> enemiesInRange = GetEnemiesInRange();
+        //private Enemy FindFurthestEnemyInRange()
+        //{
+        //    //puts all enemies in range of the tower, in a list
+        //    List<Enemy> enemiesInRange = GetEnemiesInRange();
 
-            //checks if there are enemies in range
-            if (enemiesInRange.Count == 0)
-            {
-                return null; //no enemies in range
-            }
+        //    //checks if there are enemies in range
+        //    if (enemiesInRange.Count == 0)
+        //    {
+        //        return null; //no enemies in range
+        //    }
 
-            Enemy furthestEnemy = enemiesInRange[0];
-            float furthestDistance = CalculateDistanceAlongTrack(furthestEnemy);
-            foreach (Enemy enemy in enemiesInRange)
-            {
-                float distance = CalculateDistanceAlongTrack(enemy);
-                if (distance > furthestDistance)
-                {
-                    furthestEnemy = enemy;
-                    furthestDistance = distance;
-                }
-            }
-            return furthestEnemy;
-        }
+        //    Enemy furthestEnemy = enemiesInRange[0];
+        //    float furthestDistance = CalculateDistanceAlongTrack(furthestEnemy);
+        //    foreach (Enemy enemy in enemiesInRange)
+        //    {
+        //        float distance = CalculateDistanceAlongTrack(enemy);
+        //        if (distance > furthestDistance)
+        //        {
+        //            furthestEnemy = enemy;
+        //            furthestDistance = distance;
+        //        }
+        //    }
+        //    return furthestEnemy;
+        //}
        
 
-        private float CalculateDistanceAlongTrack(Enemy enemy)
-        {
-            //TODO: Gør så den tracker hvor langt hver enemy er på graphen
-            return 0f;
-        }
+        //private float CalculateDistanceAlongTrack(Enemy enemy)
+        //{
+        //    //TODO: Gør så den tracker hvor langt hver enemy er på graphen
+        //    return 0f;
+        //}
     }
 }
